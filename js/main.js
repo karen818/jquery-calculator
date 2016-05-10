14cf69254ee8568a6d12e7c2187a271bae5bc5ca
karen818 <klc0818@outlook.com>
2016-05-10 13:56:27 -0500

    Adds more js functions

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
-// $(function(){
-    var $buttons = $('div.buttons');
+var $buttons = $('div.buttons');
+var operator = null; //best practice to set to null; specifically nothing
+var operatorUsed = false;
 
-    $buttons.on('click', 'span', function(){
-        var isOperator = $(this).hasClass('operator');
-        var button = $(this);
+$buttons.on('click', 'span', function(){
+    var isOperator = $(this).hasClass('operator');
+    var button = $(this);
+    var display = $('div#screen');
 
-        if(isOperator){
-            
+    if(button.text() === 'C'){
+        display.text('');
+    }
+
+    if(isOperator && operatorUsed === false){
+        if((button.text() !== 'C') || (button.text() !== '=')){
+            operatorUsed = true;
+            operator = button.text();
+            display.append(button.text());
         }
+
+        if(button.text() === '='){
+            operatorUsed = false;
+            var expression = display.text().split(operator);
+        }
+
+        if(operator === '+'){
+            var result = parseInt(expression[0]) + parseInt(expression[1]);
+            display.text(result);
+        }
+        //\xF7 for divide symbol
 
         if(!isOperator){
-            console.log(button.text());
+            display.append(button.text());
         }
-    });
-// });
+    }
+});
