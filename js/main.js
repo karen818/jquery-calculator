df92af9b791817ac698931e0ca779a4e7581b0aa
karen818 <klc0818@outlook.com>
2016-05-27 11:45:23 -0500

    Updates js file; getting closer on the calc function working

--- js/main.js
+++ js/main.js
@@ -3,25 +3,50 @@ $(function(){
     var operator = null; //best practice to set to null; specifically nothing
     var operatorUsed = false;
     var display = $('#screen');
+    var expression = display.text().split(operator);
+    var result = 0;
+
+    $('#cancel').click(function(){
+        display.text('');
+        operatorUsed = false;
+    });
+
+    $('#calc').click(function(){
+        //if operator is addition, adds the two numbers
+        if(operator === '+'){
+            result = parseInt(expression[0]) + parseInt(expression[1]);
+        }
+        //if operator is subtraction, subtracts num2 from num1
+        if(operator === '-'){
+            result = parseInt(expression[0]) - parseInt(expression[1]);
+        }
+        //if operator is multiplication, multiplies the two numbers
+        if(operator === 'x'){
+            result = parseInt(expression[0]) * parseInt(expression[1]);
+        }
+        //if operator is division, divides num1 by num2; if num2 is 0,
+        //diplays an error
+        if(operator === '\xF7'){
+            if (parseInt(expression[1]) === 0){
+                display.text("Error");
+            }
+            else{
+                result = parseInt(expression[0]) / parseInt(expression[1]);
+
+            }
+        }
+        display.text(result);
+    });
 
 
-    $buttons.on('click', 'span', function(){
+    $buttons.on('click', 'span:not(#cancel):not(#calc)', function(){
         var isOperator = $(this).hasClass('operator');
         var button = $(this);
-        var expression = display.text().split(operator);
-        var num1 = parseInt(expression[0]);
-        var num2 = parseInt(expression[1])
-        console.log(num1, num2);
-        var result = 0;
 
-        if((button.text() === 'C')){
-            display.text('');
-        }
 
         if(!isOperator){
             display.append(button.text());
         }
-
 
         //test if button pressed is an operator and hasn't been used
         if(isOperator && operatorUsed === false){
@@ -30,34 +55,37 @@ $(function(){
             if((button.text() !== 'C') || (button.text() !== '=')){
                 operatorUsed = true;
                 operator = button.text();
-                console.log(operator);
                 display.append(button.text());
             }
+
+            //if operator is addition, adds the two numbers
+            if(operator === '+'){
+                result = parseInt(expression[0]) + parseInt(expression[1]);
+            }
+            //if operator is subtraction, subtracts num2 from num1
+            if(operator === '-'){
+                result = parseInt(expression[0]) - parseInt(expression[1]);
+            }
+            //if operator is multiplication, multiplies the two numbers
+            if(operator === 'x'){
+                result = parseInt(expression[0]) * parseInt(expression[1]);
+            }
+            //if operator is division, divides num1 by num2; if num2 is 0,
+            //diplays an error
+            if(operator === '\xF7'){
+                if (parseInt(expression[1]) === 0){
+                    display.text("Error");
+                }
+                else{
+                    result = parseInt(expression[0]) / parseInt(expression[1]);
+
+                }
+            }
+
 
         }
     });
 
-    // //if operator is addition, adds the two numbers
-    // if(operator === '+'){
-    //     result = parseInt(expression[0]) + parseInt(expression[1]);
-    // }
-    // //if operator is subtraction, subtracts num2 from num1
-    // if(operator === '-'){
-    //     result = num1 - num2;
-    // }
-    //
-    // if(operator === 'x'){
-    //     result = num1 * num2;
-    // }
-    //
-    // if(operator === '\xF7'){
-    //     if (num2 === 0){
-    //         display.text("Error");
-    //     }
-    //     else{
-    //         result = num1 / num2;
-    //
-    //     }
-    // }
+
 
 });//end main function
